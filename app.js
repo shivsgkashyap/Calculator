768e2f2e71ec1f5b856a74a07dd7d64d02384045
Shiv Kashyap <ssgkashyap@outlook.com>
2022-10-10 14:20:34 +0100

    Keyboard input functionality

--- app.js
+++ app.js
@@ -84,3 +84,20 @@ function evaluate() {
 function roundResult(number) {
   return Math.round(number * 1000) / 1000;
 }
+
+function handleKeyboardInput(e) {
+  if (e.key >= 0 && e.key <= 9) appendNumber(e.key);
+  if (e.key === ".") appendPoint();
+  if (e.key === "=" || e.key === "Enter") evaluate();
+  if (e.key === "Backspace") deleteNumber();
+  if (e.key === "Escape") clear();
+  if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/")
+    setOperation(convertOperator(e.key));
+}
+
+function convertOperator(keyboardOperator) {
+  if (keyboardOperator === "/") return "÷";
+  if (keyboardOperator === "*") return "×";
+  if (keyboardOperator === "-") return "−";
+  if (keyboardOperator === "+") return "+";
+}
