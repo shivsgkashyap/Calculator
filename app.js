faca82d17b6b47c01c0a688edbf22ffe1cc4e6db
Shiv Kashyap <ssgkashyap@outlook.com>
2022-10-10 14:20:49 +0100

    Defined functions for calculations

--- app.js
+++ app.js
@@ -101,3 +101,19 @@ function convertOperator(keyboardOperator) {
   if (keyboardOperator === "-") return "−";
   if (keyboardOperator === "+") return "+";
 }
+
+function add(a, b) {
+  return a + b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
