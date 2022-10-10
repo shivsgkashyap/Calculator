3b27584d8f216f1eea361f5d76aaa2826ddf5113
Shiv Kashyap <ssgkashyap@outlook.com>
2022-10-10 14:21:29 +0100

    Completed final evaluation calculation fn

--- app.js
+++ app.js
@@ -117,3 +117,21 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "−":
+      return substract(a, b);
+    case "×":
+      return multiply(a, b);
+    case "÷":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
